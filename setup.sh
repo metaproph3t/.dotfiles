c5d95568e3733b81e32207e4b5882d6a39982079
metaproph3t <metaproph3t@protonmail.com>
2023-07-12 00:00:00 +0000

    Add `setup.sh` that disables kloak

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# If you want the context on why kloak is included, see
+# https://www.whonix.org/wiki/Keystroke_Deanonymization.
+# I spend so much of my day at the keyboard that it's a serious quality-of-life
+# improvement to get rid of it. If you want to see the difference before you
+# disable it completely, you can do `systemctl stop kloak`
+systemctl disable kloak
